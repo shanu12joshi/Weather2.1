45fe3e1c98efcb6426e182dafeb389799c69993f
codewithshanu <blckcodr@gmail.com>
2019-02-22 13:12:06 +0530

    Semi-Final commit

---
+++ app/src/main/java/com/example/weather/ChangeCityController.java
@@ -0,0 +1,45 @@
+package com.example.weather;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.time.Instant;
+
+
+public class ChangeCityController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.change_city_layout);
+        final EditText editTextField = (EditText) findViewById(R.id.queryET);
+        ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        editTextField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                String newCity =  editTextField.getText().toString();
+                Intent newCityIntent = new Intent(ChangeCityController.this,WeatherController.class);
+                newCityIntent.putExtra("city", newCity);
+                startActivity(newCityIntent);
+
+                return false;
+            }
+        });
+    }
+}
